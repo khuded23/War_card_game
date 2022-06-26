bf8833970c10dacc4b816d53bea84fc76a3cda98
Kiran Huded <Kiran.Huded@goto.com>
2022-06-26 17:24:55 +0530

    first commit

---
+++ wargame.py
@@ -0,0 +1,11 @@
+#Create Card Class
+
+class Card():
+
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return self.rank + "of" + self.suit
+
